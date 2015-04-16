2006f7bb98160ecaae1e40c81866a6fb715a6945
Shane Gehring <shane.gehring@gmail.com>
2015-04-16 17:51:41 -0600

    Minor tweaks to layout and page content.

--- update.bat
+++ update.bat
@@ -1,3 +1,3 @@
-rem set http_proxy=http://web-proxy:8080 
-rem set https_proxy=https://web-proxy:8080 
+ set http_proxy=http://web-proxy:8080 
+ set https_proxy=https://web-proxy:8080 
 python "C:\Program Files (x86)\Google\google_appengine\appcfg.py" -A hpvball update .
