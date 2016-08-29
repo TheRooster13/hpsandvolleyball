59c654921b75046de5f4b61da72f95d061a1d89a
Shane Gehring <shane.gehring@hp.com>
2016-08-29 10:42:51 -0600

    Matched bootstrap css/js versions, formatting tweaks

--- update.bat
+++ update.bat
@@ -1,3 +1,1 @@
-rem set http_proxy=http://web-proxy:8080 
-rem set https_proxy=https://web-proxy:8080 
 python "C:\Program Files (x86)\Google\google_appengine\appcfg.py" -A hpvball --oauth2 update .
