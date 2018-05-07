2a84ad0c8aafa0cf302508011f8b595e47413e17
Bartlow <brian.bartlow@hp.com>
2018-05-07 09:40:16 -0600

    Opened the main page for something other than signups.

--- hpsandvolleyball.py
+++ hpsandvolleyball.py
@@ -151,7 +151,34 @@ class Signup(webapp2.RequestHandler):
             else:
                 entry.committed = False
             entry.put()
-            self.redirect('/')
+            self.redirect('signup')
+	
+	def get(self):
+        # Filter for this year only
+        now = datetime.datetime.today()
+
+        # Get committed entries list
+        qry_c = Entry.query(ancestor=db_key(now.year))
+        qry_c = qry_c.filter(Entry.committed == True)
+        qry_c = qry_c.order(Entry.date)
+        entries_c = qry_c.fetch(100)
+
+        # See if user is logged in and signed up
+        login_info = get_login_info(self)
+        user = users.get_current_user()
+        player = get_player(self)
+        template_values = {
+            'year': get_year_string(),
+            'page': 'signup',
+            'user': user,
+            'entries_c': entries_c,
+            'is_signed_up': player is not None,
+            'player': player,
+            'login': login_info,
+        }
+
+        template = JINJA_ENVIRONMENT.get_template('signup.html')
+        self.response.write(template.render(template_values))
 
 class Unsignup(webapp2.RequestHandler):
     """
@@ -168,7 +195,7 @@ class Unsignup(webapp2.RequestHandler):
             for entry in entries:
                 if entry.player.identity == user.user_id(): 
                     entry.key.delete()
-        self.redirect('/')
+        self.redirect('signup')
 
 class Info(webapp2.RequestHandler):
     """
