b07ffda20a01be42afeac9a29285d2057da9e758
Bartlow <brian.bartlow@hp.com>
2018-05-14 11:40:09 -0600

    Bug Fix

--- hpsandvolleyball.py
+++ hpsandvolleyball.py
@@ -589,10 +589,11 @@ class Scheduler(webapp2.RequestHandler):
                 slots_needed -= 1
 
         # If we reach this point, we have a valid schedule! Save it to the database.
-        # First delete any existing schedule for this week
+        # First delete any existing schedule for this week (in case the scheduler runs more than once)
         qry = Schedule.query(ancestor=db_key(year))
         qry = qry.filter(Schedule.week == week)
         results = qry.fetch()
+        print(results)
         for r in results:
             r.key.delete()
         
