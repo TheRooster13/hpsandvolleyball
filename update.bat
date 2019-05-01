df4dd31c18fce7396d70b5b7aeefbd8d385253b2
Linn Kropf <linn.kropf@hp.com>
2019-05-01 13:38:29 -0600

    added REST endpoints to various requests. converted tabs to spaces. resolved merge conflicts.

--- update.bat
+++ update.bat
@@ -1,1 +1,1 @@
-gcloud app deploy app.yaml index.yaml cron.yaml --project hpsandvolleyball
+~/Downloads/google-cloud-sdk/bin/gcloud app deploy app.yaml index.yaml cron.yaml --project hpsandvolleyball
