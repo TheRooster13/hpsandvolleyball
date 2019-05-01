d6ef536756843f700adee6ec51671de8ed92c315
Linn Kropf <linn.kropf@hp.com>
2019-05-01 13:39:26 -0600

    changed path back to what it should be

--- update.bat
+++ update.bat
@@ -1,1 +1,1 @@
-~/Downloads/google-cloud-sdk/bin/gcloud app deploy app.yaml index.yaml cron.yaml --project hpsandvolleyball
+gcloud app deploy app.yaml index.yaml cron.yaml --project hpsandvolleyball
